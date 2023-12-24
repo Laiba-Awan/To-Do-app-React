2c5ba19ab469df64dbf260608342618f95433ca5
Laiba Jamil <laibajamil404@gmail.com>
2023-12-25 00:35:13 +0500

    .

--- todo-app/src/components/Header.js
+++ todo-app/src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-
 function Header () {
   return (
     <div>
       <h1 className="fw-bold main-h">
         TO-DO APP
-        <img  className="col-lg-12 text-center mt-5" src="./components/assets/todo-icon.png" alt="Icon" />
+        <img  className="col-lg-12 text-center mt-5 Iimg" src="/assets/todo-icon.png" alt="Icon" />
       </h1>
     </div>
   );
